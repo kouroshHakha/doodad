22aa2baaf86c6e25b1afe7d49ce06ae66a06991b
kourosh_hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-10-14 18:38:23 -0700

    added setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="doodad_kourosh", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)

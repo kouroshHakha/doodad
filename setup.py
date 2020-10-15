1784dce9c7be0e1792542712b5d8a9c3e747e59a
kourosh_hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-10-14 19:01:52 -0700

    setup is updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(
-    name="doodad_kourosh", # Replace with your own username
+    name="doodad", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
